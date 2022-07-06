d2266831bf16379f7e676f0bfa202d264ab074c3
Jefferson <jef9296@gmail.com>
2022-07-06 18:41:08 -0300

    Adicionada a pergunta em quem quer jogar

--- Desafio01.py
+++ Desafio01.py
@@ -5,17 +5,41 @@ print("*"*35)
 print("TIMES A SEREM SORTEADOS: \nSANTOS - FLAMENGO - PALMEIRAS\nFORTALEZA - BOTAFOGO - CORINTHIANS")
 print("*"*35)
 
-sorteio=random.randint(1,18)
-
-if sorteio == 1 and sorteio <= 3:
+sorteio=random.randint(1,19)
+nomeTime=input("EM QUAL TIME DESEJA JOGAR? ")
+if sorteio >= 1 and sorteio <= 3:
     print("O TIME SORTEADO FOI O SANTOS")
+    if nomeTime == "SANTOS":
+        print("PARABENS VOCÊ GANHOU")
+    else:
+        print("INFELIZMENTE VOCÊ PERDEU")
 elif sorteio > 3 and sorteio <= 6:
     print("O TIME SORTEADO FOI O FLAMENGO")
+    if nomeTime == "FLAMENGO":
+        print("PARABENS VOCÊ GANHOU")
+    else:
+        print("INFELIZMENTE VOCÊ PERDEU")
 elif sorteio > 6 and sorteio <= 9:
     print("O TIME SORTEADO FOI O PALMEIRAS")
+    if nomeTime == "PALMEIRAS":
+        print("PARABENS VOCÊ GANHOU")
+    else:
+        print("INFELIZMENTE VOCÊ PERDEU")
 elif sorteio > 9 and sorteio <= 12:
     print("O TIME SORTEADO FOI O FORTALEZA")
+    if nomeTime == "FORTALEZA":
+        print("PARABENS VOCÊ GANHOU")
+    else:
+        print("INFELIZMENTE VOCÊ PERDEU")
 elif sorteio > 12 and sorteio <= 15:
     print("O TIME SORTEADO FOI O BOTAFOGO")
+    if nomeTime == "BOTAFOGO":
+        print("PARABENS VOCÊ GANHOU")
+    else:
+        print("INFELIZMENTE VOCÊ PERDEU")
 elif sorteio > 15 and sorteio <= 18:
     print("O TIME SORTEADO FOI O CORINTHIANS")
+    if nomeTime == "CORINTHIANS":
+        print("PARABENS VOCÊ GANHOU")
+    else:
+        print("INFELIZMENTE VOCÊ PERDEU")

594d021373868437518f6ffd8c71286ae46b8d6c
Jefferson <jef9296@gmail.com>
2022-07-08 20:38:51 -0300

    Ídentificando os acertos

--- Desafio02.py
+++ Desafio02.py
@@ -1,35 +1,52 @@
-#serão sorteados 6 números de 1 até 50, o usuário recebe a premiação de acordo com a quantidade
-# de acertos, segue abaixo a tabela. O usuário pode fazer uma aposta no valor entre 50,00 e 100,00
-# que dará direito a escolher 4 números, ele pode apostas 101,00 até 150,00 que dará direito a escolher
-# 5 números e apostas maiores que 151,00 dará direito a 6 números
-
 import random
-nSorteados=[]
-nEscolhidos=[]
-cont=0
 
-while True:
-    valAposta=float(input("QUAL VALOR DESEJA APOSTAR? "))
+nSorteados = []
+nEscolhidos = []
+cont = 0
+ap=0
 
-    if valAposta >= 50 and valAposta <= 100:
-        aposta1 = int(input("ESCOLHA 4 NÚMEROS: "))
+valAposta = float(input("QUAL VALOR DESEJA APOSTAR? "))
+if valAposta >= 50 and valAposta <= 100:
+    while ap < 4:
+        aposta1 = int(input(f"ESCOLHA 4 NÚMEROS: "))
+        ap += 1
         nEscolhidos.append(aposta1)
-    elif valAposta >= 101 and valAposta <= 150:
-        aposta2= int(input("ESCOLHA 5 NÚMEROS: "))
+elif valAposta >= 101 and valAposta <= 150:
+    while ap < 5:
+        aposta2 = int(input("ESCOLHA 5 NÚMEROS: "))
+        ap += 1
         nEscolhidos.append(aposta2)
-    elif valAposta >= 151:
-        aposta3= int(input("ESCOLHA 6 NÚMEROS: "))
+elif valAposta >= 151:
+    while ap < 6:
+        aposta3 = int(input("ESCOLHA 6 NÚMEROS: "))
+        ap += 1
         nEscolhidos.append(aposta3)
-    else:
-        print("VALOR DA APOSTA NÃO É ACEITO!!")
-    print(nEscolhidos)
+else:
+    print("VALOR DA APOSTA NÃO É ACEITO!!")
+nEscolhidos.sort()
+print("-"*25)
+print(f"SEU JOGO: {nEscolhidos}")
+print("-"*25)
 
-    numSorteio = random.randint(1,50)
+for i in range(51):
+    numSorteio = random.randint(1, 50)
     if numSorteio not in nSorteados:
         nSorteados.append(numSorteio)
         cont += 1
     if cont >= 6:
         break
-print("-"*50)
+
+print("-" * 50)
+nSorteados.sort()
 print(f"NÚMEROS SORTEADOS: {nSorteados}")
-print("-"*50)
+print("-" * 50)
+
+for i in nSorteados:
+    if i in nEscolhidos:
+        print(f"VOCÊ ACERTOU OS NÚMEROS: ", i, end=" ")
+
+
+
+
+
+

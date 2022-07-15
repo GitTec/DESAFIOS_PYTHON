8ca7dcb99c4f071fdf01093fa0ae3c7b410a9fa1
Jefferson <jef9296@gmail.com>
2022-07-14 21:13:26 -0300

    Calculando o valor final

--- Desafio04.py
+++ Desafio04.py
@@ -4,6 +4,7 @@
 # o se o usuário ganhar pode optar por continuar e o valor da aposta será igual ao montante ganho na rodada anterior
 
 #choice-->A sequência pode ser uma string, um intervalo, uma lista, uma tupla ou qualquer outro tipo de sequência.
+import random
 
 print("<->"*10)
 print("     DESAFIO CAÇA NÍQUEL")
@@ -12,42 +13,42 @@ listaSort=["🍎", "🎃", "🍋", "🍒", "🤡"]
 print(listaSort)
 print("<->"*10)
 
-import random
-st=random.choice(listaSort)
-st2=random.choice(listaSort)
-st3=random.choice(listaSort)
-print("RODANDO....")
-print("")
-print("-"*30)
-print(f">>>>>{st, st2, st3}<<<<<")
-print("-"*30)
-#valGanho=[]
-
 while True:
     valAposta = int(input("QUANTO DESEJA APOSTAR? "))
     print()
+    st=random.choice(listaSort)
+    st2=random.choice(listaSort)
+    st3=random.choice(listaSort)
+    print("RODANDO....")
+    print()
+    print("-"*30)
+    print(f">>>>>{st, st2, st3}<<<<<")
+    print("-"*30)
+
     if st == st2 == st3 == "🤡":
-        ganho=(print(f"CORINGOUUUU EMM, VOCÊ GANHOU {valAposta * 3}"))
- #       valGanho.append(ganho)
-    if st == st2 == st3:
-        ganho2=(print(f"TODOS IGUAIS VOCÊ GANHOU {valAposta * 2}R$"))
-  #      valGanho.append(ganho2)
-    elif st == st2:
-        ganho3=(print(f"2 PRIMEIROS IGUAIS, VOCÊ GANHOU {valAposta * 2}R$"))
-   #     valGanho.append(ganho3)
-    elif st2 == st3:
-        ganho4=(print(f"2 ÚLTIMOS IGUAIS, VOCÊ GANHOU {valAposta * 2}R$"))
-    #    valGanho.append(ganho4)
+        valAposta = valAposta * 3
+        print(f"CORINGOUUUU EMM, VOCÊ GANHOU {valAposta}R$")
+    elif st == st2 == st3:
+        valAposta = valAposta * 2
+        print(f"TODOS IGUAIS VOCÊ GANHOU {valAposta}R$")
+    elif ((st == st2) and st3 == "🤡") or ((st == st3) and st2 == "🤡") or (st == "🤡" and (st2 == st3)):
+        valAposta = valAposta * 2
+        print(f"2 IGUAIS E UM PALHAÇO, VOCÊ GANHOU {valAposta}R$")
+    elif (st == st2 == "🤡") or (st2 == st3 == "🤡") or (st == st3 == "🤡"):
+        valAposta = valAposta * 2
+        print(f"2 PALHAÇOS E UM, VOCÊ GANHOU {valAposta}R$")
     else:
-        print("XIIIII UMA SEQUÊNCIA DIFERENTE, VOCÊ PERDEU TUDO")
+        print("XIII UMA SEQUÊNCIA DIFERENTE, VOCÊ PERDEU TUDO")
         quit()
     print()
     nov = input("DESEJA JOGAR NOVAMENTE? (S) <-ou-> (N) ")
-    if nov == "S" or nov=="s":
+    if nov == "S" or nov == "s":
         continue
     else:
-        print("$$-FIM DE JOGO-$$")
-     #   print(f"VOCÊ GANHOU {sum(valGanho)}")
+        print("-"*30)
+        print("     $$-FIM DE JOGO-$$")
+        print(f"VOCÊ GANHOU UM TOTAL DE {valAposta}R$")
+        print("-"*30)
         break
 
 
